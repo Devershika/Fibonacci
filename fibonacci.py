51554c391346b92baffecf773b4e2fef4a1e0ea5
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2026-03-25 11:08:18 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Function to print Fibonacci series
+void fibonacci(int n) {
+    int a = 0, b = 1, next;
+
+    printf("Fibonacci Series: ");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+}
+
+int main() {
+    int n;
+
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    fibonacci(n);
+
+    return 0;
+}
